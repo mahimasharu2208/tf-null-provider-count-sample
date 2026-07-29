b89c8f16d9277a9c491e0b17eb3d33aec8bcaae4
Mahima <mahima.sharu@hashicorp.com>
2026-07-29 12:08:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "example" {
-  count = "3"
+  count = "4"
 
   provisioner "local-exec" {
-    command = "echo Hello from instance" $count.index
+    command = "echo Hello from instance ${count.index}"
   }
 }
